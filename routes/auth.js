f391cbf070ecd3e4596fe45fad94079e37153d41
chavincc <cc6chavin@gmail.com>
2020-06-26 12:59:00 +0700

    feat(auth): remove unnecessary route

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  loginController,
-  logoutController,
-  getAuthController,
-} = require("../controllers/auth");
+const { loginController } = require("../controllers/auth");
 
 router.post("/login", loginController);
-router.get("/logout", logoutController);
-router.get("/", getAuthController);
 
 module.exports = router;
